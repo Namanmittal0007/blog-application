7a748bcaae6b1bb61964759bdb2d5ae10159e4c0
Naman Mittal <nmittal@monq.ad>
2022-05-09 17:58:10 +0530

    adding health-check api

--- src/main/java/com/naman/blogapplication/repository/UserRepo.java
+++ src/main/java/com/naman/blogapplication/repository/UserRepo.java
@@ -4,5 +4,5 @@ import com.naman.blogapplication.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //@Repository
-public interface UsersRepo extends JpaRepository<User,Integer> {
+public interface UserRepo extends JpaRepository<User,Integer> {
 }
